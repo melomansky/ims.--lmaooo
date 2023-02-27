5a888a2a9d8f654998dc6c563aa47f92e4940f2a
ims.--lmaooo <prosto.fasterr@gmail.com>
2023-02-27 07:54:12 +0000

    update config.py

--- config.py
+++ config.py
@@ -2,8 +2,11 @@ import os # Для операционной системы
 from dotenv import load_dotenv # Для переменных
 from pyrogram import Client # Импорт класса
 from pytgcalls import PyTgCalls # Импорт класса
+
+
 if os.path.exists('.env'):
     load_dotenv('.env')
+
 
 # Считаем данные из переменных .env
 SESSION_NAME = os.getenv("SESSION_NAME", "")
@@ -11,6 +14,7 @@ API_ID       = os.getenv("API_ID", "")
 API_HASH     = os.getenv("API_HASH", "")
 PREFIX       = os.getenv("PREFIX", "")
 
+# plugins/profile/update_bio.py
 PATH_TO_PHOTO = os.getenv("PATH_TO_PHOTO", "")
 PATH_TO_FONT = os.getenv("PATH_TO_FONT", "")
 PATH_TO_SAVE_PHOTO = os.getenv("PATH_TO_SAVE_PHOTO", "")
--- plugins/profile/update_bio.py
+++ plugins/profile/update_bio.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime, timedelta # Импорт времени
 from apscheduler.schedulers.asyncio import AsyncIOScheduler # многопоточность
 from PIL import Image, ImageDraw, ImageFont # Редактирования картинок
@@ -19,11 +20,19 @@ async def update_profile():
 
     draw.text((x, y), text, font=font)
     image.save(PATH_TO_SAVE_PHOTO)
+    upd_prf = (
+        f"time: {fdate} I am a person who loves jokes😋"
+        f" own: t.me/+TXWxsgXM3R0yNDIy"
+    )
+    await client.update_profile(bio=upd_prf)
     photos = [p async for p in client.get_chat_photos("me")]
-    await client.update_profile(bio="time: {} I am a person who loves jokes😋 own: t.me/+TXWxsgXM3R0yNDIy".format(fdate))
-    await client.set_profile_photo(photo=PATH_TO_SAVE_PHOTO)
-    await client.delete_profile_photos(photos[0].file_id)
+    if len(photos) > 0: # Если в photos есть картинка
+        await client.delete_profile_photos(photos[0].file_id)
+        await asyncio.sleep(1)
+        await client.set_profile_photo(photo=PATH_TO_SAVE_PHOTO)
+    else:
+        await client.set_profile_photo(photo=PATH_TO_SAVE_PHOTO)
 
 scheduler = AsyncIOScheduler()
-scheduler.add_job(update_profile, "interval", seconds=60)
-scheduler.start()
+scheduler.add_job(update_profile, "interval", seconds=10)
+scheduler.start() # Запускает отдельный поток
